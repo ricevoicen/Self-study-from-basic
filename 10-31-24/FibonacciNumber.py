eb5e62c745335af93ddffc22d2a071854f55cf87
ricevoicen <ricevoicen@gmail.com>
2024-10-31 22:00:00 +0800

    继续学习

--- 10-31-24/FibonacciNumber.py
+++ 10-31-24/FibonacciNumber.py
@@ -23,6 +23,7 @@ def fibonacci_number_hashu(n):
     else:
         return fibonacci_number_hashu(n-1) + fibonacci_number_hashu(n-2)
 
+#The `memo` in the code serves as a cache to store previously computed Fibonacci numbers, which helps to optimize the recursive function by avoiding redundant calculations. This technique is known as memoization.
 def fibonacci_number_hashu_memo(n, memo={}):
     if n ==0:
         return 0
@@ -30,12 +31,15 @@ def fibonacci_number_hashu_memo(n, memo={}):
         return 1
     else:
         return fibonacci_number_hashu_memo(n-1, memo) + fibonacci_number_hashu_memo(n-2, memo)
+#在代码中，`memo`的作用是作为缓存，用于存储先前计算的斐波那契数，从而通过避免重复计算来优化递归函数。这种技术被称为备忘录化（memoization）。
+
 
 for i in range(0, 18, 1):
-    print(fibonacci_number_hashu_memo(i))
+    print(fibonacci_number_hashu(i))
 
 
 # The incorrect code from the book 1.5.5
+# This is the first line of the code
 def fib(n):
     """Compute the nth Fibonacci number, for n >= 2."""
     pred, curr = 0, 1   # Fibonacci numbers 1 and 2
@@ -47,3 +51,4 @@ def fib(n):
 
 result = fib(8)
 #书中的错误代码 1.5.5
+#就是本代码第一行的那个
